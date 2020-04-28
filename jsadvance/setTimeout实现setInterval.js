81230292f1ac08dcbd4ff57fc00f99588d1968b1
liujun <13381035404@163.com>
2020-04-28 11:04:32 +0800

    添加一道手写题

---
+++ jsadvance/setTimeout实现setInterval.js
@@ -0,0 +1,14 @@
+function mySetInterval(fn, delay) {
+
+    const temp = () => {
+        setTimeout(temp, delay);
+        fn();
+    }
+
+    setTimeout(temp, delay);
+
+}
+
+mySetInterval(() => {
+    console.log(111);
+}, 1000);
