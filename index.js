dc641251062df357a9794de3aa6f79cbb7b88a72
liujun <13381035404@163.com>
2020-07-17 10:11:13 +0800

    每日力扣35

--- index.js
+++ index.js
@@ -1,30 +1,23 @@
 
-/**
- * @param {number[]} nums
- * @return {number}
- */
-var longestConsecutive = function(nums) {
-    nums = nums.sort((a,b) => a-b);
-    let maxLen = 1;
-    let res = 1;
+function promise(url) {
+    let obj = {};
 
-    for(let i=0; i<nums.length - 1; i++) {
-        if(nums[i] + 1 === nums[i+1]) {
-            maxLen += 1;
-            if(i === nums.length-2) {
-                res = Math.max(res, maxLen);
-            }
+    return new Promise((resolve, reject) => {
+        if(obj[url]) {
+            resolve(obj[url]);
         }else {
-            res = Math.max(res, maxLen);
-            maxLen = 1;
-            console.log(nums.length - i);
-            if(nums.length - i - 1 <= res){
-                break;
-            }
+            setTimeout(() => {
+                obj[url] = url + 'lj';
+                resolve(url + 'lj');
+            }, 2000)
         }
-    }
+    });
+}
 
+promise('1').then(res => {
     console.log(res);
-};
+})
 
-longestConsecutive([1,2,3,4, 6,7,8, 10, 11, 13,14,15,16,17,18,19]);
+promise('1').then(res => {
+    console.log(res);
+})
