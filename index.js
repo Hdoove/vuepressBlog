45d6696f19a80cbe7fead98a2992e34adcbd1677
liujun <13381035404@163.com>
2020-07-24 09:49:13 +0800

    查漏补缺

--- index.js
+++ index.js
@@ -1,15 +1,3 @@
-/**
- * @param {number[]} numbers
- * @return {number}
- */
-var minArray = function (numbers) {
-    for (let i = 0; i < numbers.length - 1; i++) {
-        if (numbers[i] > numbers[i + 1]) {
-            return numbers[i + 1];
-        }
-    }
+var a = function(){}
 
-    return numbers[0];
-};
-
-console.log(minArray([2, 2, 2, 0, 1]));
+console.log(a.prototype)
