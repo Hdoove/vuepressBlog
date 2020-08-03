be82f70188194975d88c0113db2144b87cc99255
liujun <13381035404@163.com>
2020-08-03 10:02:00 +0800

    类型转换

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
 /**
- * @param {number[]} target
- * @param {number[]} arr
- * @return {boolean}
+ * @param {number[]} candidates
+ * @param {number} target
+ * @return {number[][]}
  */
-var canBeEqual = function(target, arr) {
-    target = target.sort((a,b) => a -b).toString();
-    arr = arr.sort((a,b) => a -b).toString();
+var combinationSum2 = function (candidates, target) {
+    var dp = []
+    //先排序解决顺序问题 例 （1，2）（2，1）
+    candidates.sort((a, b) => a - b)
+    for (let i = 0; i <= target; i++) {
+        dp[i] = new Set()
+    }
+    dp[0].add('')
+    for (let c of candidates) {
+        for (let i = target; i >= c; i--) {
+            for (item of dp[i - c]) {
+                console.log(dp)
+                dp[i].add(item + ',' + c);
+            }
+        }
+    }
+    //最后把Set 转成 Array 
+    return Array.from(dp[target]).map(item => item.slice(1).split(','))
 
-    return target === arr;
+
 };
 
-console.log(canBeEqual([3,7,9], [3,7,11]));
+combinationSum2([1,2,3,4], 4);
