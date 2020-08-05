847b2c373e80712a594325836e770ee6992512b0
liujun <13381035404@163.com>
2020-08-05 10:21:23 +0800

    每日力扣404

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
-/**
- * @param {number[]} candidates
- * @param {number} target
- * @return {number[][]}
- */
-var combinationSum2 = function (candidates, target) {
-    var dp = []
-    //先排序解决顺序问题 例 （1，2）（2，1）
-    candidates.sort((a, b) => a - b)
-    for (let i = 0; i <= target; i++) {
-        dp[i] = new Set()
+
+class Parent {
+    constructor(name) {
+        this.name = name;
+        this.age = 25;
     }
-    dp[0].add('')
-    for (let c of candidates) {
-        for (let i = target; i >= c; i--) {
-            for (item of dp[i - c]) {
-                console.log(dp)
-                dp[i].add(item + ',' + c);
-            }
-        }
+
+    getName() {
+        console.log(this.name)
+        return this.name;
     }
-    //最后把Set 转成 Array 
-    return Array.from(dp[target]).map(item => item.slice(1).split(','))
 
+    setName(str) {
+        this.name = str;
+    }
+}
 
-};
+class Child extends Parent {
+    constructor(name) {
+        super(name);
+    }
+}
 
-combinationSum2([1,2,3,4], 4);
+const child = new Child('lj');
+
+child.getName();
+child.setName('hd');
+child.getName()
